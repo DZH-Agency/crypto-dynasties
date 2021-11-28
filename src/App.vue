093ba13c692eab26ba3018766e49714931f4788b
serhii.liamtsev <serhii.liamtsev@itechcraft.com>
2021-11-29 01:07:18 +0200

    Footer desktop

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,15 @@
   <div id="app">
     <common-navbar/>
     <div class="content">
-<!--      <common-header/>-->
-<!--      <young-crypto/>-->
-<!--      <show-case/>-->
-<!--      <our-mission/>-->
-<!--      <artist-section/>-->
-<!--      <questions-section/>-->
+      <common-header/>
+      <young-crypto/>
+      <show-case/>
+      <our-mission/>
+      <artist-section/>
+      <questions-section/>
       <dont-miss/>
     </div>
+    <common-footer/>
   </div>
 </template>
 
@@ -23,9 +24,11 @@ import OurMission from "@/components/OurMission";
 import ArtistSection from "@/components/ArtistSection";
 import QuestionsSection from "@/components/QuestionsSection";
 import DontMiss from "@/components/DontMiss";
+import CommonFooter from "@/components/CommonFooter";
 export default {
   name: 'App',
   components: {
+    CommonFooter,
     DontMiss,
     QuestionsSection,
     ArtistSection,
---
+++ src/components/CommonFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="footer-wrapper">
+    <div class="container">
+      <div class="footer">
+        {{ '2021 Copyrights © CryptoDynasties. \n All rights reserved.' }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CommonFooter"
+}
+</script>
+
+<style scoped lang="scss">
+@import "~@/assets/scss/components/CommonFooter.scss";
+</style>
